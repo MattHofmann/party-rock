5f78f14ee7ef5452f2ff7cdf074932207dc7cd45
matthiashofmann <mhofm@me.com>
2016-09-06 01:10:03 +0900

    add UI Elements

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Matthias Hofmann on 06.09.16.
+//  Copyright © 2016 MatthiasHofmann. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
